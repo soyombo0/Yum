69ac32080fb107e266f5becf4c7556669905a577
soyombo <soyombo0@naver.com>
2023-02-02 00:04:23 +0300

    Initial Commit

---
+++ Yum/YumApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumApp.swift
+//  Yum
+//
+//  Created by Soyombo Mantaagiin on 2.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct YumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
